c981c21945feea56c1f1d0ed489232434a3cd755
Givenazo <givenveiko@gmail.com>
2025-04-16 22:05:26 +0200

    Added the SADC Logo on all pages

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\AdminDashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
 
 Route::get('/dashboard', function () {
